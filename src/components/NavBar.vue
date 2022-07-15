741b739ff4a77d7f7039121cc91852a643f9c777
anigandini <anigandini@gmail.com>
2022-07-15 11:10:32 -0300

    nav bar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,20 +1,21 @@
 <template>
-    <nav>
-        <a href="https://www.mailup.es/" target="__blank">
-            <img alt="Mail up logo" src="@/assets/logo.png"/>
-        </a>
-    </nav>
+  <nav>
+    <a href="https://www.mailup.es/" target="__blank">
+      <img alt="Mail up logo" src="@/assets/logo.png" />
+    </a>
+  </nav>
 </template>
 <style scoped>
-    nav {
-      width: 100%;
-      display: flex;
-      margin: 0;
-    }
-    nav a:hover {
-        background: transparent;
-    }
-    nav a img{
-        width: 5rem;
-    }
+nav {
+  width: 100%;
+  display: flex;
+  margin: 0;
+  padding: 1rem;
+}
+nav a:hover {
+  background: transparent;
+}
+nav a img {
+  width: 5rem;
+}
 </style>
